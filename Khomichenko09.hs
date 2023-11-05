e1117c017354e3f5d1d5a8e705e48a795595119e
x0lotl <danil200319771@gmail.com>
2023-11-05 15:07:51 +0200

    feat(lab09): finish

--- Khomichenko09.hs
+++ Khomichenko09.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module HWC09 where
 
+import Data.List
 import Data.Tree
 
 -- Задача 1 -----------------------------------------			   
@@ -8,40 +9,65 @@ rank :: Tree a -> Int
 rank b = length (subForest b)
 
 -- Задача 2-----------------------------------------
+
+calculateNodeNumber:: Tree a -> Int
+calculateNodeNumber tree = if (length (subForest tree) == 0)
+                        then 1
+                    else 1 + sum (map (\el -> calculateNodeNumber el) (subForest tree))
+
 isBinomTree :: Ord a => Tree a -> Bool
-isBinomTree = undefined
+isBinomTree ts = 2 ^ (rank ts) == calculateNodeNumber ts
 
 -- Задача 3 -----------------------------------------
+
 isBinomHeap :: Ord a => Forest a -> Bool
-isBinomHeap = undefined
+isBinomHeap fs = all (== True) (map (\tree -> isBinomTree tree) fs)
 
 -- Задача 4 -----------------------------------------
-combineTrees :: Ord a => Tree a -> Tree a -> Tree a
-combineTrees = undefined
+-- combineTrees :: Ord a => Tree a -> Tree a -> Tree a
+combineTrees tree treeToCombine = if (rootLabel tree < rootLabel treeToCombine)
+                                        then Node (rootLabel tree) ([treeToCombine] ++ (subForest tree) ) 
+                                  else Node (rootLabel treeToCombine) ([tree] ++ (subForest treeToCombine) )
+        
 
 -- Задача 5 -----------------------------------------
 extractMin :: Ord a => Forest a -> a
-extractMin  = undefined
+extractMin forest = minimum (map (rootLabel) forest)
 
 -- Задача 6-----------------------------------------
 mergeHeaps :: Ord a => Forest a -> Forest a -> Forest a
-mergeHeaps = undefined
+mergeHeaps h []= h
+mergeHeaps [] heap= heap
+mergeHeaps h@(t:ts) heap@(t':ts')| rank t < rank t' = t : mergeHeaps ts heap| rank t' < rank t = t' : mergeHeaps ts' h| otherwise = mergeHeaps (mergeHeaps ts ts') [combineTrees t t']
 
 -- Задача 7-----------------------------------------
 insert :: Ord a => a -> Forest a -> Forest a
-insert = undefined
+insert value forest = mergeHeaps [Node value []] forest
 
 -- Задача 8-----------------------------------------
 deleteMin :: Ord a => Forest a -> Forest a
-deleteMin = undefined
+deleteMin heap = mergeHeaps heapp mrh 
+        where
+                mrt = head $ dropWhile (\t -> rootLabel t /= extractMin heap) heap
+                mrh = reverse $ subForest mrt
+                heapp = filter (\t -> t /= mrt) heap
+
 
 -- Задача 9-----------------------------------------
 binomSort :: Ord a => [a] -> [a]
-binomSort  = undefined
+binomSort [] = []
+binomSort bs = minValue : binomSort (delete (minValue) (bs))
+    where
+      heap (b:bs) = Node b [] : heap bs
+      heap [] = []
+      minValue = extractMin $ heap bs
 
 -- Задача 10 -----------------------------------------
 toBinary :: Forest a -> [Int]
-toBinary = undefined
+toBinary h = reverse $ treeBinary [0..] h
+        where
+                treeBinary _ [] = []
+                treeBinary (n:ns) t'@(t:ts)| n == rank t = 1 : (treeBinary ns ts)| otherwise   = 0 : (treeBinary ns t')
 
 -----------------------------------------------------  
 -- Приклади деяких дерев...
@@ -103,3 +129,5 @@ h7 = [Node 4 [Node 4 [Node 12 [Node 16 []],
                       Node 5 []],
               Node 6 [Node 8 []],
               Node 10 []]]  
+
+
